adbc597557ffba2ebd93fef93e83e3af56e0876a
Christopher Trevino <chrisdadef@gmail.com>
2021-03-21 20:03:26 -0500

    Did more damn reading than working today which isn't bad because i still am confused by react redux and how ACA teaches us how to use itgit add .

--- hacker_news_clone/src/App.js
+++ hacker_news_clone/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Lists from "./Components/List";
 
 //DEBATING HOW AND WHERE TO SET UP THE STATE, IM ALSO GONNA DO THIS IN THE MORE RECENT WAY AND THAT IS FUNCTIONAL COMPONENTS!!!!!
 
 function App() {
   return <div className="App">
-    
+    <Lists/>
   </div>;
 }
 
---
+++ hacker_news_clone/src/Components/List.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <div>
+      <h1>this is the lists component!</h1>
+    </div>
+  );
+};
+
+export default List;
